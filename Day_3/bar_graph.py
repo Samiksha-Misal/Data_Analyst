4d078bb26caf2220dbeb6673d21ddf7ce3770b89
Samiksha-Misal <samikshamisal22@gmail.com>
2025-01-31 12:12:23 +0530

    Bar Graph

--- Day_3/bar_graph.py
+++ Day_3/bar_graph.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 Fruits = ["Apple","Banana","Orange","Mango","Grapes"]
 Quantity = [10,20,15,25,33]
 
-plt.bar(Fruits,Quantity,width=0.4,color = ["yellow","green","orange","blue","red"],align = "center",edgecolor="black" ,linewidth = 3,linestyle = ":",alpha = 0.6)
+plt.bar(Fruits, Quantity, width=0.4, color = ["yellow","green","orange","blue","red"], align = "center", edgecolor="black", linewidth = 3, linestyle = ":", alpha = 0.6)
 
 plt.title("Fruits vs Quantity",fontsize=15)
 plt.xlabel("Fruits" ,fontsize=15)
