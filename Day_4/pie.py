64d8c1a12d6cd8f7514342af60f4ede7a8ee176b
Samiksha-Misal <samikshamisal22@gmail.com>
2025-02-04 11:47:38 +0530

    Add: File

--- Day_4/pie.py
+++ Day_4/pie.py
@@ -5,13 +5,12 @@ Mark = [65,70,80,75,90]
 Marks = [90,80,85,95]
 
 myexplode = [0.2,0,0,0]
-mycolors = ["yellow","pink","green","blue"]
-c = ["magenta","orange","red","yellow","green"]
+mycolors = ["yellow","pink","green","orange"]
+#c = ["magenta","orange","red","yellow","green"]
 
 plt.pie(Marks, labels=Subjects, startangle=90, explode=myexplode, shadow=True, colors=mycolors, autopct="%0.2f%%", radius=1.2
-    , labeldistance=1.3, textprops={"fontsize":15,}, counterclock=False, wedgeprops={"linewidth":4,"edgecolor":"grey"})
+    , labeldistance=1.2, textprops={"fontsize":10}, counterclock=False, wedgeprops={"linewidth":2,"edgecolor":"grey"})
 
-plt.pie(Mark, radius=0.5, colors=c, startangle=90, autopct="%0.2f%%", textprops={"fontsize":8})
-plt.title("Marks in different Subject", fontsize=18,pad=20, color="brown")
-plt.legend(title = "Four Subjects:", loc=1, bbox_to_anchor=(1.5, 1))
+# plt.pie(Mark, radius=0.5, colors=c, startangle=90, autopct="%0.2f%%", textprops={"fontsize":8})
+plt.title("Marks in different Subject", fontsize=15,pad=20, color="black", fontweight="bold")
 plt.show()
--- Day_4/plot.py
+++ Day_4/plot.py
@@ -6,13 +6,15 @@ sales = [160,150,140,145,175,165,180]
 price = [174,160,185,150,160,175,190]
 
 plt.subplot(1,2,1)
-plt.plot(days, sales, marker="*",  ms = 15, mec="brown", mfc="pink", linestyle="dashed", color="green", linewidth="1")
-plt.plot(days,price, marker="*",  ms = 15, mec="black", mfc="pink", linestyle="dashed", color="blue", linewidth="1")
+plt.plot(days, sales, marker="s",  ms = 5, mec="black", mfc="Green", linestyle="-", color="Green", linewidth="1", label = "Sales")
+plt.plot(days,price, marker="o",  ms = 5, mec="black", mfc="Lightcoral", linestyle="-", color="Lightcoral", linewidth="1", label = "Price")
 
-plt.title("Sales over Days", fontsize=15, color="brown", loc="left")
-plt.xlabel("Days", fontsize=15)
-plt.ylabel("Sales & Price", fontsize=15)
-plt.grid(axis = 'y', color="grey", linestyle='--' ,linewidth=0.5)
+plt.legend(fontsize = 8)
+plt.title("Sales over Days", fontsize=15, color="Black", loc="center", fontweight = "bold")
+plt.xticks(rotation = 45)
+plt.xlabel("Days", fontsize=11, color = "grey")
+plt.ylabel("Sales & Price", fontsize = 11, color = "grey")
+plt.grid(axis = 'both', color="grey", linestyle='-' ,linewidth=0.5, alpha  = 0.4)
 
 
 #plot:2
@@ -20,10 +22,11 @@ subjects = ["DBMS","CC","OS","AIML","DSA","Python","TOC"]
 marks = [75,80,65,74,90,84,70]
 
 plt.subplot(1,2,2)
-plt.plot(subjects ,marks, marker="*",  ms = 15, mec="black", mfc="pink", linestyle="dashed", color="blue", linewidth="1")
+plt.plot(subjects ,marks, marker="s",  ms = 5, mec="black", mfc="orange", linestyle="-", color="orange", linewidth="1", label = "Marks")
 
-plt.title("Marks in different subjects", fontsize=15, color="brown", loc="left")
-plt.xlabel("Subjects", fontsize=15)
-plt.ylabel("Marks", fontsize=15)
-plt.grid(axis = 'y', color="grey", linestyle='--' ,linewidth=0.5)
+plt.legend(fontsize = 8, frameon = False, loc = "upper left")
+plt.title("Marks in different subjects", fontsize=15, color="black", loc="center", fontweight = "bold")
+plt.xlabel("Subjects", fontsize=11, color = "grey")
+plt.ylabel("Marks", fontsize = 11, color = "grey")
+plt.grid(axis = 'both', color="grey", linestyle='-' ,linewidth=0.5, alpha = 0.4)
 plt.show()
